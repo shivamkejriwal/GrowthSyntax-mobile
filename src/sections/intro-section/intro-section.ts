2acbd28788acbeca759e61a1ce1c6f61be460453
Shivam Kejriwal <shkejriwal@paypal.com>
2017-11-05 01:09:08 -0800

    Adding graph to intro section.

--- src/sections/intro-section/intro-section.ts
+++ src/sections/intro-section/intro-section.ts
@@ -26,7 +26,64 @@ export class IntroSection {
                           && data.fundamentals.length > 0);
     if(dataComplete) {
       console.log('IntroSection-dataComplete', data);
+      this.buildChart();
     }
+  }
+
+  buildChart() {
+    var ctx = document.getElementById("polarArea");
+    let data = {
+        labels: [
+            'Health',
+            'Value',
+            'Performance',
+            'Dividend',
+            'Management'
+        ],
+        datasets: [{
+            data: [5, 3, 7, 8, 9],
+            fillOpacity: .3,
+            backgroundColor: [
+                "rgba(255, 0, 0, 0.3)",
+                "rgba(100, 255, 0, 0.3)",
+                "rgba(200, 50, 255, 0.3)",
+                "rgba(300, 100, 255, 0.3)",
+                "rgba(0, 100, 255, 0.3)"
+            ]
+        }],
+    };
+    let options = {
+        responsive: true,
+        legend: {
+            display: true,
+            position: 'left'
+        },
+        title: {
+            display: false,
+            text: 'Summary'
+        },
+        scale: {
+            display: true,
+            gridLines: {
+                display: false,
+                drawBorder: false,
+                lineWidth: 0
+            },
+            angleLines: {
+                display: false
+            },
+            ticks: {
+                display: false,
+                suggestedMax: 10
+            }
+        }
+    };
+    var chart = new Chart(ctx, {
+        type: 'polarArea',
+        data: data,
+        options: options
+    });
+    this.charts.push(chart);
   }
 
   reset() {
