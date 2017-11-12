cd1bdd10b952cfbf6a68c079a8c208cfb359eca8
Shivam Kejriwal <shivamkejriwal@gmail.com>
2017-11-12 00:47:07 -0800

    testing inappbrowser for article

--- src/pages/article/article.ts
+++ src/pages/article/article.ts
@@ -23,20 +23,21 @@ export class ArticlePage {
     console.log('goToOriginalContent',{url});
     const isMac = this.platform.is('ios');
     const desktop = this.platform.is('core');
-    if (isMac || desktop) {
-      window.open(url);
-    }
-    else {
-      this.browserTab.isAvailable()
-      .then((isAvailable: boolean) => {
-        console.log('browserTab-isAvailable' + isAvailable);
-        if (isAvailable) {
-          this.browserTab.openUrl(url);
-        } else {
-          const browser = this.iab.create(url);
-        }
-      });
-    }
+    window.open(url, '_blank');
+    // if (isMac || desktop) {
+    //   window.open(url);
+    // }
+    // else {
+    //   this.browserTab.isAvailable()
+    //   .then((isAvailable: boolean) => {
+    //     console.log('browserTab-isAvailable' + isAvailable);
+    //     if (isAvailable) {
+    //       this.browserTab.openUrl(url);
+    //     } else {
+    //       const browser = this.iab.create(url);
+    //     }
+    //   });
+    // }
   }
 
 }
