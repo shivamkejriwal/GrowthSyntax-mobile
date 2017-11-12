e97d18c3e53bf60b4f571b86aa0a34af497e8c22
Shivam Kejriwal <shivamkejriwal@gmail.com>
2017-11-12 00:31:21 -0800

    adding inappbrowser to article page

--- src/pages/article/article.ts
+++ src/pages/article/article.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, Platform } from 'ionic-angular';
 import { BrowserTab } from '@ionic-native/browser-tab';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'page-article',
@@ -11,6 +12,7 @@ export class ArticlePage {
   constructor(public platform: Platform,
               public navCtrl: NavController, 
               public navParams: NavParams,
+              private iab: InAppBrowser,
               private browserTab: BrowserTab) {
     this.article = this.navParams.get('article');
     console.log('ArticlePage', this.article);
@@ -30,6 +32,8 @@ export class ArticlePage {
         console.log('browserTab-isAvailable' + isAvailable);
         if (isAvailable) {
           this.browserTab.openUrl(url);
+        } else {
+          const browser = this.iab.create(url);
         }
       });
     }
